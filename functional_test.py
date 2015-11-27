1c9dc3cea3210e301f7aeb77d20d7f29a7df241e
Frank <susocket@sina.com>
2015-11-27 16:00:15 +0800

    ch03 ok

--- functional_test.py
+++ functional_test.py
@@ -13,7 +13,7 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        print(browser.title)
+        print(self.browser.title)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
